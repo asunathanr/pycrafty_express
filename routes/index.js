6b746f2afee05a824af7d01e1e92089dddb40d5e
asunathanr <nrobert2@my.athens.edu>
2019-07-05 03:13:55 -0500

    Generated python script is now written to mcpipy directory

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('/public/index.html');
 });
 
-// Catches copy_text and writes requested text to python file.
+// Catches copy_text and writes requested text to python file in mcpipy directory.
 router.post('/copy_text', function (req, res) {
   const fs = require('fs');
-  fs.writeFile("./public/test.py", req.body.codeArea, function (err) {
+  const os = require('os');
+  let file_path = os.userInfo().homedir + "\\AppData\\Roaming\\.minecraft\\mcpipy\\gen_script.py";
+  fs.writeFile(file_path, req.body.codeArea, function (err) {
     if (err) {
       console.log(err);
     }
