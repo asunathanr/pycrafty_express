b5ae7c52f930ab9cd57f65ef002963694d9c2be7
asunathanr <nrobert2@my.athens.edu>
2019-07-07 01:27:20 -0500

    Basic version of export now works

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
-var express = require('express');
-var router = express.Router();
+// FILE: index.js
+// AUTHORS: Richie Burch, Nathan Robertson
+// PURPOSE: Detects various GET and POST requests and performs appropriate actions on them.
+
+const fs = require('fs');
+const os = require('os');
+const express = require('express');
+let router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,14 +20,18 @@ router.post('/importAction', function (req, res) {
 
 
 router.post('/exportAction', function (req, res) {
-  res.send("Export was clicked");
+  let file_path = "test.xml";
+  fs.writeFile(file_path, req.body.xmlBlockData, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  res.redirect("/");
 });
 
 
 // Catches copy_text and writes requested text to python file in mcpipy directory.
 router.post('/copy_text', function (req, res) {
-  const fs = require('fs');
-  const os = require('os');
   let file_path = os.userInfo().homedir + "\\AppData\\Roaming\\.minecraft\\mcpipy\\gen_script.py";
   fs.writeFile(file_path, req.body.codeArea, function (err) {
     if (err) {
