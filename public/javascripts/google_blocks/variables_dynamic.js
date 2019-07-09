298115bf94166549a1a8529fb574812030f28f34
Richie Burch <rburch@gmail.com>
2019-07-09 11:22:03 -0500

    trying out provided variable_dynamic.js

--- public/javascripts/google_blocks/variables_dynamic.js
+++ public/javascripts/google_blocks/variables_dynamic.js
@@ -19,168 +19,102 @@
  */
 
 /**
- * @fileoverview Variable google_blocks for Blockly.
-
- * This file is scraped to extract a .json file of block definitions. The array
- * passed to defineBlocksWithJsonArray(..) must be strict JSON: double quotes
- * only, no outside references, no functions, no trailing commas, etc. The one
- * exception is end-of-line comments, which the scraper will remove.
+ * @fileoverview Utility functions for handling variables dynamic.
+ *
  * @author duzc2dtw@gmail.com (Du Tian Wei)
  */
 'use strict';
 
-goog.provide('Blockly.Constants.VariablesDynamic');
+goog.provide('Blockly.VariablesDynamic');
 
+goog.require('Blockly.Variables');
 goog.require('Blockly.Blocks');
-goog.require('Blockly');
+goog.require('Blockly.constants');
+goog.require('Blockly.VariableModel');
+// TODO Fix circular dependencies
+// goog.require('Blockly.Workspace');
+goog.require('Blockly.Xml');
 
+
+Blockly.VariablesDynamic.onCreateVariableButtonClick_String = function(button) {
+  Blockly.Variables.createVariableButtonHandler(button.getTargetWorkspace(),
+      null, 'String');
+};
+Blockly.VariablesDynamic.onCreateVariableButtonClick_Number = function(button) {
+  Blockly.Variables.createVariableButtonHandler(button.getTargetWorkspace(),
+      null, 'Number');
+};
+Blockly.VariablesDynamic.onCreateVariableButtonClick_Colour = function(button) {
+  Blockly.Variables.createVariableButtonHandler(button.getTargetWorkspace(),
+      null, 'Colour');
+};
+/**
+ * Construct the elements (blocks and button) required by the flyout for the
+ * variable category.
+ * @param {!Blockly.Workspace} workspace The workspace containing variables.
+ * @return {!Array.<!Element>} Array of XML elements.
+ */
+Blockly.VariablesDynamic.flyoutCategory = function(workspace) {
+  var xmlList = [];
+  var button = document.createElement('button');
+  button.setAttribute('text', Blockly.Msg['NEW_STRING_VARIABLE']);
+  button.setAttribute('callbackKey', 'CREATE_VARIABLE_STRING');
+  xmlList.push(button);
+  button = document.createElement('button');
+  button.setAttribute('text', Blockly.Msg['NEW_NUMBER_VARIABLE']);
+  button.setAttribute('callbackKey', 'CREATE_VARIABLE_NUMBER');
+  xmlList.push(button);
+  button = document.createElement('button');
+  button.setAttribute('text', Blockly.Msg['NEW_COLOUR_VARIABLE']);
+  button.setAttribute('callbackKey', 'CREATE_VARIABLE_COLOUR');
+  xmlList.push(button);
+
+  workspace.registerButtonCallback('CREATE_VARIABLE_STRING',
+      Blockly.VariablesDynamic.onCreateVariableButtonClick_String);
+  workspace.registerButtonCallback('CREATE_VARIABLE_NUMBER',
+      Blockly.VariablesDynamic.onCreateVariableButtonClick_Number);
+  workspace.registerButtonCallback('CREATE_VARIABLE_COLOUR',
+      Blockly.VariablesDynamic.onCreateVariableButtonClick_Colour);
+
+
+  var blockList = Blockly.VariablesDynamic.flyoutCategoryBlocks(workspace);
+  xmlList = xmlList.concat(blockList);
+  return xmlList;
+};
 
 /**
- * Unused constant for the common HSV hue for all google_blocks in this category.
- * @deprecated Use Blockly.Msg['VARIABLES_DYNAMIC_HUE']. (2018 April 5)
+ * Construct the blocks required by the flyout for the variable category.
+ * @param {!Blockly.Workspace} workspace The workspace containing variables.
+ * @return {!Array.<!Element>} Array of XML block elements.
  */
-Blockly.Constants.VariablesDynamic.HUE = 310;
+Blockly.VariablesDynamic.flyoutCategoryBlocks = function(workspace) {
+  var variableModelList = workspace.getAllVariables();
 
-Blockly.defineBlocksWithJsonArray([ // BEGIN JSON EXTRACT
-  // Block for variable getter.
-  {
-    "type": "variables_get_dynamic",
-    "message0": "%1",
-    "args0": [{
-      "type": "field_variable",
-      "name": "VAR",
-      "variable": "%{BKY_VARIABLES_DEFAULT_NAME}"
-    }],
-    "output": null,
-    "style": "variable_dynamic_blocks",
-    "helpUrl": "%{BKY_VARIABLES_GET_HELPURL}",
-    "tooltip": "%{BKY_VARIABLES_GET_TOOLTIP}",
-    "extensions": ["contextMenu_variableDynamicSetterGetter"]
-  },
-  // Block for variable setter.
-  {
-    "type": "variables_set_dynamic",
-    "message0": "%{BKY_VARIABLES_SET}",
-    "args0": [{
-      "type": "field_variable",
-      "name": "VAR",
-      "variable": "%{BKY_VARIABLES_DEFAULT_NAME}"
-    },
-    {
-      "type": "input_value",
-      "name": "VALUE"
+  var xmlList = [];
+  if (variableModelList.length > 0) {
+    if (Blockly.Blocks['variables_set_dynamic']) {
+      var firstVariable = variableModelList[variableModelList.length - 1];
+      var gap = 24;
+      var blockText = '<xml>' +
+          '<block type="variables_set_dynamic" gap="' + gap + '">' +
+          Blockly.Variables.generateVariableFieldXmlString(firstVariable) +
+          '</block>' +
+          '</xml>';
+      var block = Blockly.Xml.textToDom(blockText).firstChild;
+      xmlList.push(block);
     }
-    ],
-    "previousStatement": null,
-    "nextStatement": null,
-    "style": "variable_dynamic_blocks",
-    "tooltip": "%{BKY_VARIABLES_SET_TOOLTIP}",
-    "helpUrl": "%{BKY_VARIABLES_SET_HELPURL}",
-    "extensions": ["contextMenu_variableDynamicSetterGetter"]
-  }
-]); // END JSON EXTRACT (Do not delete this comment.)
-
-/**
- * Mixin to add context menu items to create getter/setter google_blocks for this
- * setter/getter.
- * Used by google_blocks 'variables_set_dynamic' and 'variables_get_dynamic'.
- * @mixin
- * @augments Blockly.Block
- * @package
- * @readonly
- */
-Blockly.Constants.VariablesDynamic.CUSTOM_CONTEXT_MENU_VARIABLE_GETTER_SETTER_MIXIN = {
-  /**
-   * Add menu option to create getter/setter block for this setter/getter.
-   * @param {!Array} options List of menu options to add to.
-   * @this Blockly.Block
-   */
-  customContextMenu: function(options) {
-    // Getter google_blocks have the option to create a setter block, and vice versa.
-    if (!this.isInFlyout) {
-      var opposite_type;
-      var contextMenuMsg;
-      var id = this.getFieldValue('VAR');
-      var variableModel = this.workspace.getVariableById(id);
-      var varType = variableModel.type;
-      if (this.type == 'variables_get_dynamic') {
-        opposite_type = 'variables_set_dynamic';
-        contextMenuMsg = Blockly.Msg['VARIABLES_GET_CREATE_SET'];
-      } else {
-        opposite_type = 'variables_get_dynamic';
-        contextMenuMsg = Blockly.Msg['VARIABLES_SET_CREATE_GET'];
-      }
-
-      var option = {enabled: this.workspace.remainingCapacity() > 0};
-      var name = this.getField('VAR').getText();
-      option.text = contextMenuMsg.replace('%1', name);
-      var xmlField = document.createElement('field');
-      xmlField.setAttribute('name', 'VAR');
-      xmlField.setAttribute('variabletype', varType);
-      xmlField.appendChild(document.createTextNode(name));
-      var xmlBlock = document.createElement('block');
-      xmlBlock.setAttribute('type', opposite_type);
-      xmlBlock.appendChild(xmlField);
-      option.callback = Blockly.ContextMenu.callbackFactory(this, xmlBlock);
-      options.push(option);
-    } else {
-      if (this.type == 'variables_get_dynamic' ||
-       this.type == 'variables_get_reporter_dynamic') {
-        var renameOption = {
-          text: Blockly.Msg.RENAME_VARIABLE,
-          enabled: true,
-          callback: Blockly.Constants.Variables.RENAME_OPTION_CALLBACK_FACTORY(this)
-        };
-        var name = this.getField('VAR').getText();
-        var deleteOption = {
-          text: Blockly.Msg.DELETE_VARIABLE.replace('%1', name),
-          enabled: true,
-          callback: Blockly.Constants.Variables.DELETE_OPTION_CALLBACK_FACTORY(this)
-        };
-        options.unshift(renameOption);
-        options.unshift(deleteOption);
+    if (Blockly.Blocks['variables_get_dynamic']) {
+      variableModelList.sort(Blockly.VariableModel.compareByName);
+      for (var i = 0, variable; variable = variableModelList[i]; i++) {
+        var blockText = '<xml>' +
+            '<block type="variables_get_dynamic" gap="8">' +
+            Blockly.Variables.generateVariableFieldXmlString(variable) +
+            '</block>' +
+            '</xml>';
+        var block = Blockly.Xml.textToDom(blockText).firstChild;
+        xmlList.push(block);
       }
     }
-  },
-  onchange: function() {
-    var id = this.getFieldValue('VAR');
-    var variableModel = this.workspace.getVariableById(id);
-    if (this.type == 'variables_get_dynamic') {
-      this.outputConnection.setCheck(variableModel.type);
-    } else {
-      this.getInput('VALUE').connection.setCheck(variableModel.type);
-    }
   }
+  return xmlList;
 };
-
-/**
-  * Callback for rename variable dropdown menu option associated with a
-  * variable getter block.
-  * @param {!Blockly.Block} block The block with the variable to rename.
-  * @return {!function()} A function that renames the variable.
-  */
-Blockly.Constants.VariablesDynamic.RENAME_OPTION_CALLBACK_FACTORY = function(block) {
-  return function() {
-    var workspace = block.workspace;
-    var variable = block.getField('VAR').getVariable();
-    Blockly.Variables.renameVariable(workspace, variable);
-  };
-};
-
-/**
- * Callback for delete variable dropdown menu option associated with a
- * variable getter block.
- * @param {!Blockly.Block} block The block with the variable to delete.
- * @return {!function()} A function that deletes the variable.
- */
-Blockly.Constants.VariablesDynamic.DELETE_OPTION_CALLBACK_FACTORY = function(block) {
-  return function() {
-    var workspace = block.workspace;
-    var variable = block.getField('VAR').getVariable();
-    workspace.deleteVariableById(variable.getId());
-    workspace.refreshToolboxSelection();
-  };
-};
-
-Blockly.Extensions.registerMixin('contextMenu_variableDynamicSetterGetter',
-    Blockly.Constants.VariablesDynamic.CUSTOM_CONTEXT_MENU_VARIABLE_GETTER_SETTER_MIXIN);
