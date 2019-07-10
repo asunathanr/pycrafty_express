16042396d2ff23268039237ef28e6f536ecf8fc0
Richie Burch <rburch@gmail.com>
2019-07-10 12:52:22 -0500

    rearranged minecraft blocks and minecraft.js file to match positions.

--- public/javascripts/minecraft_blocks/minecraft.js
+++ public/javascripts/minecraft_blocks/minecraft.js
@@ -5,192 +5,215 @@
 
 
 //JSON array of blocks
-Blockly.defineBlocksWithJsonArray([{
-  // mc.getBlock()
-  "type": "get_block",
-  "message0": "Get Block %1 Position: %2",
-  "args0": [
-    {
-      "type": "input_dummy"
-    },
-    {
-      "type": "input_value",
-      "name": "vec",
-      "check": "Vector"
-    }
-  ],
-  "output": "Block",
-  "colour": 230,
-  "tooltip": "Get block type from provided coordinates.",
-  "helpUrl": ""
-},
-{
-  // mc.setBlock()
-  "type": "set_block",
-  "message0": "Set a single block. %1 Position: %2 Block: %3",
-  "args0": [
-    {
-      "type": "input_dummy"
-    },
-    {
-      "type": "input_value",
-      "name": "vec",
-      "check": "Vector"
-    },
-    {
-      "type": "input_value",
-      "name": "block",
-      "check": "Block"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 230,
-  "tooltip": "Set a single block.",
-  "helpUrl": ""
-},
-{
-  //mc.setBlocks()
-  "type": "set_blocks",
-  "message0": "Set cube of blocks. %1 Start Position: %2 End Position: %3 Block: %4",
-  "args0": [
-    {
-      "type": "input_dummy"
-    },
-    {
-      "type": "input_value",
-      "name": "vec1",
-      "check": "Vector"
-    },
-    {
-      "type": "input_value",
-      "name": "vec2",
-      "check": "Vector"
-    },
-    {
-      "type": "input_value",
-      "name": "block",
-      "check": "Block"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 230,
-  "tooltip": "",
-  "helpUrl": ""
-},
-{
-  // mc.getPlayerId()
-  "type": "get_player_id",
-  "message0": "Get player id.",
-  "output": "Number",
-  "colour": 230,
-  "tooltip": "Get players id as int.",
-  "helpUrl": ""
-},
-{
-  // mc.getPlayerEntityIds()
-  "type": "get_player_entity_ids",
-  "message0": "Get player ids.",
-  "output": "List",
-  "colour": 230,
-  "tooltip": "",
-  "helpUrl": ""
-},
-{
-  // mc.getHeight()
-  "type": "get_height",
-  "message0": "Get Height %1 Position: %2",
-  "args0": [
-    {
-      "type": "input_dummy"
-    },
-    {
-      "type": "input_value",
-      "name": "vec",
-      "check": "Vector"
-    }
-  ],
-  "output": "Number",
-  "colour": 230,
-  "tooltip": "Get height of tallest non-air block at position.",
-  "helpUrl": ""
-},
-{
-  // mc.getBlockWithData()
-  "type": "get_block_with_data",
-  "message0": "Get block type and id. %1 Position: %2",
-  "args0": [
-    {
-      "type": "input_dummy"
-    },
-    {
-      "type": "input_value",
-      "name": "vec",
-      "check": "Vector"
-    }
-  ],
-  "output": "Block",
-  "colour": 230,
-  "tooltip": "",
-  "helpUrl": ""
-},
-{
-  // mc.postToChat()
-  "type": "post_to_chat",
-  "message0": "Post to chat. %1 %2 %3",
-  "args0": [
-    {
-      "type": "input_dummy"
-    },
-    {
-      "type": "input_value",
-      "name": "TOPOST"
-    },
-    {
-      "type": "input_dummy"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 230,
-  "tooltip": "Post a message to in game chat.",
-  "helpUrl": ""
-},
-{
-  // mc.getBlocks()
-  "type": "get_blocks",
-  "message0": "Get list of block types. %1 Start Position: %2 End Position: %3",
-  "args0": [
-    {
-      "type": "input_dummy"
-    },
-    {
-      "type": "input_value",
-      "name": "vec1",
-      "check": "Vector"
-    },
-    {
-      "type": "input_value",
-      "name": "vec2",
-      "check": "Vector"
-    }
-  ],
-  "output": "List",
-  "colour": 230,
-  "tooltip": "Gets types of blocks in a cuboid. Returns a list of ints.",
-  "helpUrl": ""
-}]);
+Blockly.defineBlocksWithJsonArray([
+  {
+    // mc.postToChat()
+    "type": "post_to_chat",
+    "message0": "Post to chat. %1 %2 %3",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "TOPOST"
+      },
+      {
+        "type": "input_dummy"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "Post a message to in game chat.",
+    "helpUrl": ""
+  },
+  {
+    // mc.setBlock()
+    "type": "set_block",
+    "message0": "Set a single block. %1 Position: %2 Block: %3",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "vec",
+        "check": "Vector"
+      },
+      {
+        "type": "input_value",
+        "name": "block",
+        "check": "Block"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "Set a single block.",
+    "helpUrl": ""
+  },
+  {
+    //mc.setBlocks()
+    "type": "set_blocks",
+    "message0": "Set cube of blocks. %1 Start Position: %2 End Position: %3 Block: %4",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "vec1",
+        "check": "Vector"
+      },
+      {
+        "type": "input_value",
+        "name": "vec2",
+        "check": "Vector"
+      },
+      {
+        "type": "input_value",
+        "name": "block",
+        "check": "Block"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    // mc.getBlock()
+    "type": "get_block",
+    "message0": "Get Block %1 Position: %2",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "vec",
+        "check": "Vector"
+      }
+    ],
+    "output": "Block",
+    "colour": 230,
+    "tooltip": "Get block type from provided coordinates.",
+    "helpUrl": ""
+  },
+  {
+    // mc.getBlockWithData()
+    "type": "get_block_with_data",
+    "message0": "Get block type and id. %1 Position: %2",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "vec",
+        "check": "Vector"
+      }
+    ],
+    "output": "Block",
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    // mc.getBlocks()
+    "type": "get_blocks",
+    "message0": "Get list of block types. %1 Start Position: %2 End Position: %3",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "vec1",
+        "check": "Vector"
+      },
+      {
+        "type": "input_value",
+        "name": "vec2",
+        "check": "Vector"
+      }
+    ],
+    "output": "List",
+    "colour": 230,
+    "tooltip": "Gets types of blocks in a cuboid. Returns a list of ints.",
+    "helpUrl": ""
+  },
+  {
+    // mc.getHeight()
+    "type": "get_height",
+    "message0": "Get Height %1 Position: %2",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "vec",
+        "check": "Vector"
+      }
+    ],
+    "output": "Number",
+    "colour": 230,
+    "tooltip": "Get height of tallest non-air block at position.",
+    "helpUrl": ""
+  },
+  {
+    // mc.getPlayerId()
+    "type": "get_player_id",
+    "message0": "Get player id.",
+    "output": "Number",
+    "colour": 230,
+    "tooltip": "Get players id as int.",
+    "helpUrl": ""
+  },
+  {
+    // mc.getPlayerEntityIds()
+    "type": "get_player_entity_ids",
+    "message0": "Get player ids.",
+    "output": "List",
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  }
+]);
 
 // Code generators
+Blockly.Python['post_to_chat'] = function (block) {
+  let value_topost = block.getFieldValue('TOPOST');
+  return 'mc.postToChat(' + '"' + value_topost + '"' + ')\n';
+};
+Blockly.Python['set_block'] = function(block) {
+  var value_vec = Blockly.Python.valueToCode(block, 'vec', Blockly.Python.ORDER_ATOMIC);
+  var value_block = Blockly.Python.valueToCode(block, 'block', Blockly.Python.ORDER_ATOMIC);
+  var code = 'mc.setBlock('+value_vec+', '+value_block+')\n';
+  return code;
+};
+Blockly.Python['set_blocks'] = function(block) {
+  var value_vec1 = Blockly.Python.valueToCode(block, 'vec1', Blockly.Python.ORDER_ATOMIC);
+  var value_vec2 = Blockly.Python.valueToCode(block, 'vec2', Blockly.Python.ORDER_ATOMIC);
+  var value_block = Blockly.Python.valueToCode(block, 'block', Blockly.Python.ORDER_ATOMIC);
+  var code = 'mc.setBlocks('+value_vec1+', '+value_vec2+', '+value_block+')\n';
+  return code;
+};
 Blockly.Python['get_block'] = function(block) {
     var value_vec = Blockly.Python.valueToCode(block, 'vec', Blockly.Python.ORDER_ATOMIC);
     var code = 'mc.getBlock('+value_vec+')\n';
     // TODO: Change ORDER_NONE to the correct strength.
     return [code, Blockly.Python.ORDER_NONE];
 };
-
-
+Blockly.Python['get_block_with_data'] = function(block) {
+  var value_vec = Blockly.Python.valueToCode(block, 'vec', Blockly.Python.ORDER_ATOMIC);
+  var code = 'mc.getBlockWithData('+value_vec+')\n';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_NONE];
+};
 Blockly.Python['get_blocks'] = function(block) {
     var value_vec1 = Blockly.Python.valueToCode(block, 'vec1', Blockly.Python.ORDER_ATOMIC);
     var value_vec2 = Blockly.Python.valueToCode(block, 'vec2', Blockly.Python.ORDER_ATOMIC);
@@ -198,52 +221,19 @@ Blockly.Python['get_blocks'] = function(block) {
     // TODO: Change ORDER_NONE to the correct strength.
     return [code, Blockly.Python.ORDER_NONE];
 };
-
-Blockly.Python['get_block_with_data'] = function(block) {
-    var value_vec = Blockly.Python.valueToCode(block, 'vec', Blockly.Python.ORDER_ATOMIC);
-    var code = 'mc.getBlockWithData('+value_vec+')\n';
-    // TODO: Change ORDER_NONE to the correct strength.
-    return [code, Blockly.Python.ORDER_NONE];
-  };
-
-  Blockly.Python['get_height'] = function(block) {
+Blockly.Python['get_height'] = function(block) {
     var value_vec = Blockly.Python.valueToCode(block, 'vec', Blockly.Python.ORDER_ATOMIC);
     var code = 'mc.getHeight('+value_vec+')\n';
     // TODO: Change ORDER_NONE to the correct strength.
     return [code, Blockly.Python.ORDER_NONE];
-  };
-
+};
+Blockly.Python['get_player_id'] = function (block) {
+  var code = 'mc.getPlayerId()\n';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_NONE];
+};
 Blockly.Python['get_player_entity_ids'] = function (block) {
     var code = 'mc.getPlayerEntityIds()\n';
     // TODO: Change ORDER_NONE to the correct strength.
     return [code, Blockly.Python.ORDER_NONE];
-};
-
-Blockly.Python['get_player_id'] = function (block) {
-    var code = 'mc.getPlayerId()\n';
-    // TODO: Change ORDER_NONE to the correct strength.
-    return [code, Blockly.Python.ORDER_NONE];
-};
-
-Blockly.Python['post_to_chat'] = function (block) {
-    let value_topost = block.getFieldValue('TOPOST');
-    return 'mc.postToChat(' + '"' + value_topost + '"' + ')\n';
-};
-
-
-Blockly.Python['set_blocks'] = function(block) {
-    var value_vec1 = Blockly.Python.valueToCode(block, 'vec1', Blockly.Python.ORDER_ATOMIC);
-    var value_vec2 = Blockly.Python.valueToCode(block, 'vec2', Blockly.Python.ORDER_ATOMIC);
-    var value_block = Blockly.Python.valueToCode(block, 'block', Blockly.Python.ORDER_ATOMIC);
-    var code = 'mc.setBlocks('+value_vec1+', '+value_vec2+', '+value_block+')\n';
-    return code;
-
-};
-
-
-  Blockly.Python['set_block'] = function(block) {
-    var value_vec = Blockly.Python.valueToCode(block, 'vec', Blockly.Python.ORDER_ATOMIC);
-    var value_block = Blockly.Python.valueToCode(block, 'block', Blockly.Python.ORDER_ATOMIC);
-    var code = 'mc.setBlock('+value_vec+', '+value_block+')\n';
-    return code;
 };
