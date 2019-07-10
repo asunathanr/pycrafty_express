4295006a10320ee94001891a7a4fd2f80f18b95c
Richie Burch <rburch@gmail.com>
2019-07-10 16:18:56 -0500

    updated get_block generator to add newline only if it is not called by another block

--- public/javascripts/minecraft_blocks/minecraft.js
+++ public/javascripts/minecraft_blocks/minecraft.js
@@ -187,12 +187,13 @@ Blockly.defineBlocksWithJsonArray([
 // Code generators
 Blockly.Python['post_to_chat'] = function (block) {
   let value_topost = block.getFieldValue('TOPOST');
-  return 'mc.postToChat(' + '"' + value_topost + '"' + ')\n';
+  var code = 'mc.postToChat(' + '"' + value_topost + '"' + ')\n';
+  return code;
 };
 Blockly.Python['set_block'] = function(block) {
   var value_vec = Blockly.Python.valueToCode(block, 'vec', Blockly.Python.ORDER_ATOMIC);
   var value_block = Blockly.Python.valueToCode(block, 'block', Blockly.Python.ORDER_ATOMIC);
-  var code = 'mc.setBlock('+value_vec+', '+value_block+')\n';
+  var code = 'mc.setBlock('+value_vec+', '+value_block+')';
   return code;
 };
 Blockly.Python['set_blocks'] = function(block) {
@@ -204,9 +205,14 @@ Blockly.Python['set_blocks'] = function(block) {
 };
 Blockly.Python['get_block'] = function(block) {
     var value_vec = Blockly.Python.valueToCode(block, 'vec', Blockly.Python.ORDER_ATOMIC);
-    var code = 'mc.getBlock('+value_vec+')\n';
+    var code = 'mc.getBlock('+value_vec+')';
+    if(block.getParent() === null) {
     // TODO: Change ORDER_NONE to the correct strength.
-    return [code, Blockly.Python.ORDER_NONE];
+      return [code+'\n', Blockly.Python.ORDER_NONE];
+    } else {
+      return [code, Blockly.Python.ORDER_NONE];
+    }
+    
 };
 Blockly.Python['get_block_with_data'] = function(block) {
   var value_vec = Blockly.Python.valueToCode(block, 'vec', Blockly.Python.ORDER_ATOMIC);
