042e2115a0130a96db01d94bbbc6c4cbcc0a1dd4
Richie Burch <rburch@gmail.com>
2019-07-10 15:17:39 -0500

    updated entity set position

--- public/javascripts/minecraft_blocks/entity.js
+++ public/javascripts/minecraft_blocks/entity.js
@@ -19,7 +19,29 @@ Blockly.defineBlocksWithJsonArray([
         "colour": 230,
         "tooltip": "Get position of specified player.",
         "helpUrl": ""
-      },
+    },
+    {
+        "type": "entity_set_position",
+        "message0": "Set position of %1 to %2",
+        "args0": [
+          {
+            "type": "input_value",
+            "name": "ENTITYID",
+            "check": "Number"
+          },
+          {
+            "type": "input_value",
+            "name": "VECTOR",
+            "check": "Vector"
+          }
+        ],
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 230,
+        "tooltip": "Move the specified player to the provided location.",
+        "helpUrl": ""
+    },
 
 ]);
 
@@ -31,4 +53,11 @@ Blockly.Python['entity_get_position'] = function(block) {
     var code = 'mc.entity.getPos('+value_entityid+')\n';
     // TODO: Change ORDER_NONE to the correct strength.
     return [code, Blockly.Python.ORDER_NONE];
+  };
+  Blockly.Python['entity_set_position'] = function(block) {
+    var value_entityid = Blockly.Python.valueToCode(block, 'ENTITYID', Blockly.Python.ORDER_ATOMIC);
+    var value_vector = Blockly.Python.valueToCode(block, 'VECTOR', Blockly.Python.ORDER_ATOMIC);
+    // TODO: Assemble Python into code variable.
+    var code = 'mc.entity.setPos('+value_entityid+', '+value_vector+')\n';
+    return code;
   };
