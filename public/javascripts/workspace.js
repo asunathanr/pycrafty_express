a491eb79f8543820d3c34e9b44158d13b92e2344
asunathanr <nrobert2@my.athens.edu>
2019-07-12 15:07:20 -0500

    Scrollbar now starts at leftmost, topmost position of workspace

--- public/javascripts/workspace.js
+++ public/javascripts/workspace.js
@@ -8,7 +8,7 @@ var blocklyDiv = document.getElementById('blocklyDiv');
 var mainWorkspace = Blockly.inject(blocklyDiv,
     {
         media: 'media' + "\\",
-        toolbox: document.getElementById('toolbox')
+        toolbox: document.getElementById('toolbox'),
     });
 
 var onresize = function (event) {
@@ -47,8 +47,12 @@ mainWorkspace.addChangeListener(function () {
     displayCodeInBrowser();
 });
 
-// TODO: Make scrollbars start at 0,0
+
 
 window.addEventListener('resize', onresize, false);
 onresize();
 Blockly.svgResize(mainWorkspace);
+
+// Scroll to leftmost and topmost position of workspace
+mainWorkspace.scroll(mainWorkspace.width, mainWorkspace.height);
+mainWorkspace.render();
