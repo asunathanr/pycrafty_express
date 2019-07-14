696754ad4fe94b6bedf5b8dfeead5a9b9720fec5
asunathanr <nrobert2@my.athens.edu>
2019-07-13 21:50:26 -0500

    Added zoom tooling to workspace

--- public/javascripts/workspace.js
+++ public/javascripts/workspace.js
@@ -9,6 +9,14 @@ var mainWorkspace = Blockly.inject(blocklyDiv,
     {
         media: 'media' + "\\",
         toolbox: document.getElementById('toolbox'),
+        zoom:
+            {controls: true,
+                wheel: true,
+                startScale: 1.0,
+                maxScale: 3,
+                minScale: 0.3,
+                scaleSpeed: 1.2},
+        trashcan: true
     });
 
 var onresize = function (event) {
