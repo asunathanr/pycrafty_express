54ebf121e53fada0a935b0cc87adb125cbc67075
Richie Burch <rburch@gmail.com>
2019-07-17 17:28:44 -0500

    fixed for loop count variables giving set variable warning

--- public/javascripts/google_blocks/variables_dynamic.js
+++ public/javascripts/google_blocks/variables_dynamic.js
@@ -225,6 +225,8 @@ Blockly.Extensions.register("check_for_setter", function() {
       }
     } else if ((this.getRootBlock().type === 'controls_forEach') &&(this.getRootBlock().getFieldValue("VAR") === id)) {
       this.setWarningText(null);
+    } else if ((this.getRootBlock().type === 'controls_for') &&(this.getRootBlock().getFieldValue("VAR") === id)) {
+      this.setWarningText(null);
     } else { // if there are no setters for the current variable, warn
       this.setWarningText("You need to set variable first!");
     }
